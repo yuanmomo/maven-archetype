25930f7e502ecbf3805aae36301e6f910b3a971e
yuanmomo <342398690@qq.com>
2015-02-16 12:56:21 +0800

    去掉生成代码，添加生成pom依赖

---
+++ spring-springmvc-mybatis-dwz/src/test/java/net/yuanmomo/dwz/mybatis/generator/GeneratorMain.java
@@ -0,0 +1,29 @@
+/**
+ * Project Name : Tools
+ * File Name    : MyBatisGeneratorTest.java
+ * Package Name : net.yuanmomo.tools.db.orm.mybatis
+ * Created on   : 2013-12-24下午5:13:58
+ * Author       : Hongbin Yuan
+ * Blog         : yuanmomo.net
+ * Company      : 成都逗溜网科技有限公司  
+ */
+
+package net.yuanmomo.dwz.mybatis.generator;
+
+import net.yuanmomo.mybatis.generator.MyBatisGeneratorTool;
+
+
+/**
+ * ClassName : MyBatisGeneratorTest Function : TODO ADD FUNCTION. Reason : TODO
+ * ADD REASON. Date : 2013-12-24 下午5:13:58
+ * 
+ * @author : Hongbin Yuan
+ * @version
+ * @since JDK 1.6
+ * @see
+ */
+public class GeneratorMain {
+	public static void main(String args[]) {
+		MyBatisGeneratorTool.generate("src\\test\\resources\\mybatis\\generatorConfig.xml");
+	}
+}

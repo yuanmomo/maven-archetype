23e8870ed05e0fcd5c293a3ca224f17a235f12dc
Hongbin.Yuan <yuanhongbin@foxdsp.com>
2016-05-29 15:33:35 +0800

    添加事务测试

--- framework-dwz/src/test/java/BasicTest.java
+++ framework-dwz/src/test/java/BasicTest.java
@@ -5,9 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:config/ApplicationContext.xml")
+@TransactionConfiguration(transactionManager = "txManager", defaultRollback = true)
 public class BasicTest {
 	@Autowired
 	protected TestBusiness testBusiness = null;

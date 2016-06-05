303f1883fc0fdc59c817229804adb43d08bd5e2c
Hongbin.Yuan <yuanhongbin@foxdsp.com>
2016-06-05 15:08:56 +0800

    修改 threadlocal

--- framework-init/src/main/java/net/yuanmomo/framework/util/session/HttpThreadLocal.java
+++ framework-init/src/main/java/net/yuanmomo/framework/util/session/HttpThreadLocal.java
@@ -1,6 +1,8 @@
 package net.yuanmomo.framework.util.session;
 
 import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by Hongbin.Yuan on 2015-11-14 23:41.
@@ -8,17 +10,45 @@ import javax.servlet.http.HttpSession;
 
 public class HttpThreadLocal {
     //创建线程局部变量session，用来保存 HTTP 请求中 的Session
-    public static final ThreadLocal<HttpSession> sessionLocal = new ThreadLocal();
+    public static final ThreadLocal<Map<String,Object>> THREAD_LOCAL= new ThreadLocal();
+
+    private static final String SESSION = "1";
 
 
     public static void setSession(HttpSession session) {
-        sessionLocal.set(session);
+        set(SESSION,session);
     }
 
     public static HttpSession getSession() {
-        return sessionLocal.get();
+        return (HttpSession) get(SESSION);
     }
     public static void remove(){
-        sessionLocal.remove();
+        THREAD_LOCAL.remove();
+    }
+
+    /**
+     * 设置值.
+     *
+     * @param key
+     * @return
+     */
+    private static Object get(String key) {
+        return THREAD_LOCAL.get() == null ? null : THREAD_LOCAL.get().get(key);
+    }
+
+    /**
+     * 取值.
+     *
+     * @param key
+     * @param object
+     */
+    private static void set(String key, Object object) {
+        if (THREAD_LOCAL.get() == null) {
+            THREAD_LOCAL.set(new ConcurrentHashMap<String, Object>());
+        }
+        if (object == null) {
+            return;
+        }
+        THREAD_LOCAL.get().put(key, object);
     }
 }
